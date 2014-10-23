042ee5c95ea38162ffbc57d943272eedd9797c0b
492371545 <xmyxumengying@hotmail.com>
2014-10-23 11:42:29 +0800

    Initial Commit

---
+++ RightSlide/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RightSlide
+//
+//  Created by Mengying Xu on 14-10-23.
+//  Copyright (c) 2014年 Crystal Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RightSlide/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RightSlide
+//
+//  Created by Mengying Xu on 14-10-23.
+//  Copyright (c) 2014年 Crystal Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
